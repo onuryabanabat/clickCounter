617484193565afbc00a3fe8938554045e48b0c32
Onur <onuryabanabat@gmail.com>
2017-07-23 17:03:14 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/onur/clickcounter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.onur.clickcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void display(int number){
+        TextView quantityTextView=(TextView) findViewById(R.id.textView3);
+        quantityTextView.setText(""+number);
+    }
+
+    public void submitCount(View view){
+        count++;
+        display(count);
+
+    }
+
+}
